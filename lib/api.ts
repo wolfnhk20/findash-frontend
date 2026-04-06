4e8f86cec4ca0e8d25c2a4c0e073b06be3b8cc06
wolfnhk20 <ayushkulal20@gmail.com>
2026-04-06 23:08:32 +0530

    updated api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -9,6 +9,7 @@ API.interceptors.request.use((config) => {
     const token = localStorage.getItem("token")
 
     if (token) {
+      config.headers = config.headers || {}
       config.headers.Authorization = `Bearer ${token}`
     }
   }
